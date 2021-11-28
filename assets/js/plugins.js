37734de39c432f60eb9ca0905793d3453bba2475
Hossamfahmyuiux <hossamfahmyuiux@gmail.com>
2021-11-28 06:39:35 +0200

    Blueasy-freebie code

---
+++ assets/js/plugins.js
@@ -0,0 +1,13 @@
+('.navbtn span').click(function(e){
+    $('li.button a').removeClass('active');
+    $(this).addClass('active');
+    ...
+});     
+    
+    
+     document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.twitte P', {
+        strings: ["hELLOO FDLSKFJ", "Second sentence."],
+        typeSpeed: 0
+      });
+  });
